979bb1754a8987bac137046aa693590db268e087
Olivier Hagolle <olivier.hagolle@cesbio.cnes.fr>
2014-08-05 22:06:47 +0200

    bug fixes for list mode and proxy, and integration of jake-brinkmann/LANDSAT-Download improvements

--- download_landsat_scene.py
+++ download_landsat_scene.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: iso-8859-1 -*-
-import os,sys,math,urllib2,urllib
+
+"""
+    Landsat Data download from earth explorer
+    Incorporates jake-Brinkmann improvements
+"""
+import os,sys,math,urllib2,urllib,time
 import optparse
 ###########################################################################
 class OptionParser (optparse.OptionParser):
@@ -14,17 +19,12 @@ class OptionParser (optparse.OptionParser):
  
 #############################"Connection à Earth explorer sans proxy
  
-def connect_earthexplorer_proxy(usgs,proxy):
+def connect_earthexplorer_proxy(proxy_info,usgs):
     
-     proxy_info = {
-     'user' : proxy['account'],
-     'pass' : proxy['passwd'],
-     'host' : proxy['address'],
-     'port' : proxy['port'],
-     }
+     print "proxy_info=", proxy_info
      # contruction d'un "opener" qui utilise une connexion proxy avec autorisation
-     proxy_support = urllib2.ProxyHandler({"http" : "http://%(user)s:%(pass)s@%(host)s:%(port)d" % proxy_info,
-     "https" : "http://%(user)s:%(pass)s@%(host)s:%(port)d" % proxy_info})
+     proxy_support = urllib2.ProxyHandler({"http" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info,
+     "https" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info})
      opener = urllib2.build_opener(proxy_support, urllib2.HTTPCookieProcessor)
  
      # installation
@@ -37,8 +37,15 @@ def connect_earthexplorer_proxy(usgs,proxy):
      f = opener.open('https://earthexplorer.usgs.gov/login', params)
      data = f.read()
      f.close()
- 
+
+     if data.find('You must sign in as a registered user to download data or place orders for USGS EROS products')>0 :        
+	 print "Authentification failed"
+	 sys.exit(-1)
+
+
+
      return
+ 
  
 #############################"Connection à Earth explorer sans proxy
  
@@ -53,12 +60,17 @@ def connect_earthexplorer_no_proxy(usgs):
 	print "Authentification failed"
 	sys.exit(-1)
     return
+#############################
  
-#############################"pour des gros fichiers
- 
+def sizeof_fmt(num):
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0 
+#############################
 def downloadChunks(url,rep,nom_fic):
-  """Telecharge de gros fichiers par morceaux
-     inspire de http://josh.gourneau.com
+  """ Downloads large files in pieces
+   inspired by http://josh.gourneau.com
   """
  
   try:
@@ -74,24 +86,36 @@ def downloadChunks(url,rep,nom_fic):
 	  print sys.exit(-1)
     total_size = int(req.info().getheader('Content-Length').strip())
     if (total_size<50000):
-      print "erreur : le fichier est trop petit pour etre une image landsat"
-      print url
-      sys.exit(-1)
+       print "Error: The file is too small to be a Landsat Image"
+       print url
+       sys.exit(-1)
     print nom_fic,total_size
+    total_size_fmt = sizeof_fmt(total_size)
+
     downloaded = 0
     CHUNK = 1024 * 1024 *8
     with open(rep+'/'+nom_fic, 'wb') as fp:
-      while True:
-	chunk = req.read(CHUNK)
-	downloaded += len(chunk)
-	sys.stdout.write(str(math.floor((float(downloaded) / total_size) * 100 )) +'%')
-	sys.stdout.flush()
-	if not chunk: break
-	fp.write(chunk)
-      print 'fini'
+        start = time.clock()
+        print('Downloading {0} ({1}):'.format(nom_fic, total_size_fmt))
+	while True:
+	     chunk = req.read(CHUNK)
+	     downloaded += len(chunk)
+	     done = int(50 * downloaded / total_size)
+	     sys.stdout.write('\r[{1}{2}]{0:3.0f}% {3}ps'
+                             .format(math.floor((float(downloaded)
+                                                 / total_size) * 100),
+                                     '=' * done,
+                                     ' ' * (50 - done),
+                                     sizeof_fmt((downloaded // (time.clock() - start)) / 8)))
+	     sys.stdout.flush()
+	     if not chunk: break
+	     fp.write(chunk)
   except urllib2.HTTPError, e:
-    print "HTTP Error:",e.code , url
-    return False
+       if e.code == 500:
+            pass # File doesn't exist
+       else:
+            print "HTTP Error:", e.code , url
+       return False
   except urllib2.URLError, e:
     print "URL Error:",e.reason , url
     return False
@@ -99,7 +123,7 @@ def downloadChunks(url,rep,nom_fic):
   return rep,nom_fic
  
 ######################################################################################
-###############""main
+###############main
 #################################################################################
  
 ################Lecture des arguments
@@ -115,21 +139,23 @@ else:
 	usage = "usage: %prog [options] "
 	parser = OptionParser(usage=usage)
         parser.add_option("-o", "--option", dest="option", action="store", type="choice", \
-			help="scene ou liste", choices=['scene','liste'],default=None)
+			help="scene or liste", choices=['scene','liste'],default=None)
 	parser.add_option("-l", "--liste", dest="fic_liste", action="store", type="string", \
-			help="nom du fichier liste",default=None)
+			help="liste filename",default=None)
         parser.add_option("-s", "--scene", dest="scene", action="store", type="string", \
 			help="coordonnees WRS2 de la scene (ex 198030)", default=None)
 	parser.add_option("-a", "--annee", dest="annee", action="store", type="int", \
 			help="annee")
 	parser.add_option("-d", "--doy_deb", dest="doy_deb", action="store", type="int", \
-			help="premier jour dans l'annee")
+			help="first landsat day of year for tile ")
 	parser.add_option("-f","--doy_fin", dest="doy_fin", action="store", type="int", \
-			help="dernier jour dans l'annee")
+			help="last day of year for tile")
 	parser.add_option("-u","--usgs_passwd", dest="usgs", action="store", type="string", \
 			help="USGS earthexplorer account and password file")
 	parser.add_option("-p","--proxy_passwd", dest="proxy", action="store", type="string", \
 			help="Proxy account and password file")
+	parser.add_option("--output", dest="output", action="store", type="string", \
+			help="Where to download files",default='/tmp/LANDSAT')
  
 	(options, args) = parser.parse_args()
 	parser.check_required("-o")
@@ -139,15 +165,16 @@ else:
 	    parser.check_required("-f")
 	    parser.check_required("-s")
 	    parser.check_required("-u")
+	    
 	elif options.option=='liste' :
 	    parser.check_required("-l")
     	    parser.check_required("-u")
 
- 
-rep='/tmp/LANDSAT'
+
+rep=options.output
 if not os.path.exists(rep):
     os.mkdir(rep)
-    if options.option==liste:
+    if options.option=='liste':
 	if not os.path.exists(rep+'/LISTE'):
 	     os.mkdir(rep+'/LISTE')
  
@@ -168,22 +195,20 @@ except :
 if options.proxy != None :
     try:
 	f=file(options.proxy)
-	(account,passwd)=f.readline().split(' ')
+	(user,passwd)=f.readline().split(' ')
 	if passwd.endswith('\n'):
 	    passwd=passwd[:-1]
-	proxy={'account':account,'passwd':passwd}
-	address=f.readline()
-	if address.endswith('\n'):
-	    address=address[:-1]
+	host=f.readline()
+	if host.endswith('\n'):
+	    host=host[:-1]
 	port=f.readline()
 	if port.endswith('\n'):
 	    port=port[:-1]
-	proxy={'port':port}
+	proxy={'user':user,'pass':passwd,'host':host,'port':port}
 	f.close()
     except :
 	print "error with proxy password file"
 	sys.exit(-3)
-
 ############Telechargement des produits par scene
 if options.option=='scene':
     produit='LC8'
@@ -210,11 +235,11 @@ if options.option=='scene':
 	   url="http://earthexplorer.usgs.gov/download/%s/%s/STANDARD/EE"%(repert,nom_prod)
 	   print url
 	   if not(os.path.exists(rep_scene+'/'+nom_prod+'.tgz')):
-	       try:
-		   if options.proxy!=None :
+	       if options.proxy!=None :
 		       connect_earthexplorer_proxy(proxy,usgs)
-		   else:
+	       else:
 		       connect_earthexplorer_no_proxy(usgs)
+	       try:
 		   downloadChunks(url,"%s"%rep_scene,nom_prod+'.tgz')
 	       except TypeError:
 		  print '   produit %s non trouve'%nom_prod
