4c5ca6715f12a12d245958ade03a2324b9746da6
Olivier Hagolle <olivier.hagolle@cnes.fr>
2016-10-10 23:08:20 +0200

    [ENH]changes to get the html file in case of issue with server

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 LANDSAT-Download
 ================
 
-you might also be interested by the S2-download tool to download Sentinel-2 data
-https://github.com/olivierhagolle/Sentinel-download/blob/master/README.md
+*you might also be interested by the S2-download tool to download Sentinel-2 data
+https://github.com/olivierhagolle/Sentinel-download/blob/master/README.md*
 
 The routine provided below enables to automatically download LANDSAT data, using the current (April 2014) version of EarthExplorer system. This new version (2014-08-19) does not need to provide the exact overpass date anymore, I have reused here an idea of my colleague Michel Le Page at CESBIO (Thanks Michel !), though implemented differently.
 
 It works for LANDSAT 8 and LANDSAT 5&7, but needs that the data be already online. It was true for LANDSAT 8 until September 2014, but after that date, to avoid increasing the on-line data volume indefinitly, USGS started to clean out older data to replace them by the new ones. It is also the case for the older LANDSAT satellites. Depending on the products you need, it may be necessary to first order for the production of L1T products, on the earthexplorer site http://earthexplorer.usgs.gov. And of course, you will need to have an account and password on the Earthexplorer website, and you will have to store it on the usgs.txt file. If you have an access through a proxy, you might try the -p option. it works through CNES proxy at least but was only tested there.
 
+In 2016, USGS introduced a CRSF token for the authentification. [@mkmitchell](https://github.com/mkmitchell) found a solution, which was later enhanced by [@dswanepoel](https://github.com/dswanepoel).
+
+####Examples :
 This routine may be used in three ways :
 
 - iterative search, by providing the WRS-2 coordinates of the LANDSAT scene, for instance, (198,030) for Toulouse (-s option), , the start date (-d option), and the end date (-f option). If the end date is not provided, it is replaced by today's date by default. Example:
--- download_landsat_scene.py
+++ download_landsat_scene.py
@@ -24,38 +24,48 @@ class OptionParser (optparse.OptionParser):
 #############################"Connection to Earth explorer with proxy
  
 def connect_earthexplorer_proxy(proxy_info,usgs):
-     print "Establishing connection to Earthexplorer with proxy..."    
-     # contruction d'un "opener" qui utilise une connexion proxy avec autorisation
-     proxy_support = urllib2.ProxyHandler({"http" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info,
-     "https" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info})
-     opener = urllib2.build_opener(proxy_support, urllib2.HTTPCookieProcessor)
+    print "Establishing connection to Earthexplorer with proxy..."    
+    # contruction d'un "opener" qui utilise une connexion proxy avec autorisation
+    cookies = urllib2.HTTPCookieProcessor()
+    proxy_support = urllib2.ProxyHandler({"http" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info,
+    "https" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info})
+    opener = urllib2.build_opener(proxy_support, cookies)
  
-     # installation
-     urllib2.install_opener(opener)
-
-     # parametres de connection
-     params = urllib.urlencode(dict(username=usgs['account'], password=usgs['passwd']))
+    # installation
+    urllib2.install_opener(opener)
+    # deal with csrftoken required by USGS as of 8-8-2016
+    try:
+        token = [x.value for x in cookies.cookiejar if x.name == 'csrftoken'][0]
+    except IndexError:
+        return False, "no csrftoken"
+    # parametres de connection
+    params = urllib.urlencode(dict(username=usgs['account'], password=usgs['passwd'], csrfmiddlewaretoken=token))
  
-     # utilisation
-     #f = opener.open('https://ers.cr.usgs.gov/login', params)
-     f = opener.open('https://ers.cr.usgs.gov/login', params)
-     data = f.read()
-     f.close()
+    # utilisation
+    #f = opener.open('https://ers.cr.usgs.gov/login', params)
+    f = opener.open('https://ers.cr.usgs.gov/login', params)
+    data = f.read()
+    f.close()
 
-     if data.find('You must sign in as a registered user to download data or place orders for USGS EROS products')>0 :        
+    if data.find('You must sign in as a registered user to download data or place orders for USGS EROS products')>0 :        
         print "Authentification failed"
         sys.exit(-1)
 
-     return
+    return
  
  
 #############################"Connection to Earth explorer without proxy
  
 def connect_earthexplorer_no_proxy(usgs):
-    print "Establishing connection to Earthexplorer..."
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+    cookies = urllib2.HTTPCookieProcessor()
+    opener = urllib2.build_opener(cookies)
     urllib2.install_opener(opener)
-    params = urllib.urlencode(dict(username=usgs['account'],password= usgs['passwd']))
+    # deal with csrftoken required by USGS as of 8-8-2016
+    try:
+        token = [x.value for x in cookies.cookiejar if x.name == 'csrftoken'][0]
+    except IndexError:
+        return False, "no csrftoken"
+    params = urllib.urlencode(dict(username=usgs['account'],password= usgs['passwd'], csrfmiddlewaretoken=token))
     f = opener.open("https://ers.cr.usgs.gov/login", params)
     data = f.read()
     f.close()
@@ -81,13 +91,15 @@ def downloadChunks(url,rep,nom_fic):
     req = urllib2.urlopen(url)
     #taille du fichier
     if (req.info().gettype()=='text/html'):
-      print "erreur : le fichier est au format html"
+      print "error : file is in html format, and shouldn't"
       lignes=req.read()
       if lignes.find('Download Not Found')>0 :
             raise TypeError
       else:
-	  print lignes
-	  print sys.exit(-1)
+          with open("error_output.html","w") as f:
+              f.write(lignes)
+              print "result saved in ./error_output.html"
+              print sys.exit(-1)
     total_size = int(req.info().getheader('Content-Length').strip())
     if (total_size<50000):
        print "Error: The file is too small to be a Landsat Image"
